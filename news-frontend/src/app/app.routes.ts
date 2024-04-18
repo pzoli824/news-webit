65d34dfb3cd4bc70c1e1dfe8d1f25fbcbcdd7532
Pribék Zoltán <pribekzoltan0824@gmail.com>
2024-04-18 14:42:52 +0200

    added not found page and basic bootstrap css

--- news-frontend/src/app/app.routes.ts
+++ news-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundPageComponent } from '../errors/pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
